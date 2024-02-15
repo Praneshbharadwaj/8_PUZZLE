73966b3afd689b09f75cb141a8537d25b46dfb50
srikrishnag28 <srikrishnag28@gmail.com>
2024-02-15 16:40:55 +0530

    No

--- app/app.py
+++ app/app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/solve', methods=['POST'])
+def solve_puzzle():
+    data = request.json
+    initial_state = data.get('puzzle')
+    solver = PuzzleSolver()
+    steps = solver.solve(initial_state)
+    next_step = solver.next_step(initial_state)
+    context = {
+        'steps': steps,
+        'next_step': next_step,
+    }
+    return render_template('index.html', context=context)
+
 @app.route('/shuffle')
 def shuffle():
     solver = PuzzleSolver()
